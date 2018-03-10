286ed9ea0d58fce0e3d228eb1df66d461bb0c9a5
Deformator <dammandrii@gmail.com>
2018-03-10 16:04:09 -0500

    Bootstrap connecting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,22 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from './../environments/environment';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
